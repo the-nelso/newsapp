754aa4a3bf3d9d19a22d8e869fa2bbc42369390d
Nelson Meduna <nelsonmeduna34@gmail.com>
2023-01-14 15:44:42 -0300

    * Criação de widget para textfield personalizado; * Início de tela de login (faltou navegação de telas e botão para cadastro).

--- lib/src/auth/sing_in_screen.dart
+++ lib/src/auth/sing_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/src/components/custom_text_field.dart';
 
 class SingInScreen extends StatelessWidget {
   const SingInScreen({Key? key}) : super(key: key);
@@ -21,22 +22,71 @@ class SingInScreen extends StatelessWidget {
                 vertical: 40,
               ),
               decoration: const BoxDecoration(
-                color: Colors.blueGrey,
+                color: Colors.white,
                 borderRadius: BorderRadius.vertical(
                   top: Radius.circular(45),
                 ),
               ),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   // Email
-                  TextFormField(
-                    decoration: InputDecoration(
-                      isDense: true,
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(45)),
+                  const CustomTextField(
+                    icon: Icons.email,
+                    label: "Email",
+                  ),
+
+                  // Senha
+                  const CustomTextField(
+                    icon: Icons.lock,
+                    label: "Senha",
+                    isSecret: true,
+                  ),
+                  //Entrar
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      )),
+                      child: const Text(
+                        "Entrar",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
                     ),
                   ),
-                  // Senha
-                  TextFormField(),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text("Esqueceu a senha?"),
+                    ),
+                  ),
+
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Text("Ou"),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 2,
+                        ),
+                      )
+                    ],
+                  ),
                 ],
               ),
             ),
